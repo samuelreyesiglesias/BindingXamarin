4a73d8715c0f6155c836f256cf3966acec3434fe
Samuel <samuelreyesiglesias@gmail.com>
2022-04-29 13:20:35 -0600

    Agregar archivos de proyecto.

---
+++ App2/App2/Services/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+using App2.Views;
+
+namespace App2.Services
+{
+    public class Class1
+    {
+        public string Name { get; set; } = "Samuel";
+        public string Surname { get; set; } = "Reyes";
+ 
+
+        public Command Comando { get; set; } = null;
+        public Class1()
+        {
+            Comando = new Command(MiMetodo);
+        }
+
+        public   void MiMetodo()
+        {
+
+            App.Current.MainPage.DisplayAlert("Hola", "Como estas?" + this.Name + this.Surname, "OK");
+
+        }
+
+
+    }
+}
---
+++ App2/App2/Views/MiPagina.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MiPagina : ContentPage
+    {
+        public MiPagina()
+        {
+            InitializeComponent();
+        }
+
+        //private void Button_Clicked(object sender, EventArgs e)
+        //{
+        //    DisplayAlert("Mostrando nombre", "Tu nombre es:", "Cancelar");
+        //}
+    }
+}
